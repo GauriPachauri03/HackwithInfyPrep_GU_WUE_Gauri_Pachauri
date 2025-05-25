423b1ca6265ea9d34ec034d1071eac9052d0c7a2
Gauri Pachauri <gauripachauri945@gmail.com>
2025-05-25 12:07:53 +0530

    Add files via upload

---
+++ MissingNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class MissingNumber {
+    public static void main(String[] args) {
+        int n=5;
+        int[] arr = new int[n - 1];
+        arr = new int[] { 2, 3, 5, 1 };
+
+        int missing = findMissingNumber(n, arr);
+        System.out.println(missing);
+
+    }
+    public static int findMissingNumber(int n, int[] arr) {
+        long expectedSum = (long) n * (n + 1) / 2;
+        long actualSum = 0;
+        for (int num : arr) {
+            actualSum += num;
+        }
+        return (int) (expectedSum - actualSum);
+    }
+}
